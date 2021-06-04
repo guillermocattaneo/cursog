0cb210ad375c2ca9b9447585997fa5700b26eb5a
guillermocattaneo <guillermo.cattaneo@gmail.com>
2021-06-04 00:35:53 -0300

    ModPerfil

---
+++ routers/modificarPerfil.go
@@ -0,0 +1,28 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/guillermocattaneo/cursog/bd"
+	"github.com/guillermocattaneo/cursog/models"
+)
+
+func ModificarPerfil(w http.ResponseWriter, r *http.Request) {
+	var t models.Usuario
+
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, "Datos incorrectos "+err.Error(), 400)
+		return
+	}
+
+	var status bool
+	status, err = bd.ModificoRegistro(t, IDUsuario)
+	if err != nil {
+		http.Error(w, "Ocurrio un error al intentar modificar el archivo. Reintentar. "+err.Error(), 400)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
